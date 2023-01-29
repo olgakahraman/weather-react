b7a24da0e3214fd8f54b2ede319570765c045a4b
olgakahraman <ol.khrmn@gmail.com>
2023-01-30 00:18:53 +0300

    install bootstrap and axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
 import WeatherSearch from "./WeatherSearch";
 
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <WeatherSearch />
+      <div className="container">
+        <h1>Weather App</h1>
+        <WeatherSearch />
+      </div>
+      <p>
+        Coded by Olga Dietiuk Kahraman and it is
+        <a href="https://github.com/olgakahraman/weather-react">open-sourced</a>
+      </p>
     </div>
   );
 }
