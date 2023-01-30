1e6eee9e3ee5e26bf4651952f6cfe48822c97ed4
olgakahraman <ol.khrmn@gmail.com>
2023-01-31 00:36:22 +0300

    created weather.js component and made some styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import WeatherSearch from "./WeatherSearch";
+import React from "react";
+
+import Weather from "./Weather";
+
 
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <WeatherSearch />
+        
+        <Weather />
       </div>
-      <p>
-        Coded by Olga Dietiuk Kahraman and it is
-        <a href="https://github.com/olgakahraman/weather-react">open-sourced</a>
-      </p>
+      <footer className="text-center">
+        Coded by Olga Dietiuk Kahraman and it is 
+        <a href="https://github.com/olgakahraman/weather-react"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather shadow  ps-4 bg-body-tertiary rounded">
+      <form className=" mt-4 mb-4">
+        <div className="row">
+          <div className="col-9">
+            <input
+              Type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1 className="city ">New York</h1>
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li>Wednesday 07:00</li>
+            <li>Mostly Cloudy</li>
+          </ul>
+        </div>
+        <div className="col-4 text-center">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="temperature col-6">26℃</div>
+
+        <div className="col-6 pt-4">
+          <ul>
+            <li>Details</li>
+            <li>Feels Like:    28℃</li>
+            <li>Humidity:      75%</li>
+            <li>Wind:          13 km/h</li>
+          </ul>
+        </div>
+      
+      </div>
+    </div>
+  );
+}
