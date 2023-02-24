1c303244b75dfffe26cffba589a303bf659e9fec
olgakahraman <ol.khrmn@gmail.com>
2023-02-24 13:21:15 -0500

    added target="_blank"  rel="noopener noreferrer" to links

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 
 import Weather from "./Weather";
 
-
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <div className="container">
-        
-        <Weather defaultCity="New York"/>
+        <Weather defaultCity="New York" />
       </div>
       <footer className="text-center">
-        Coded by Olga Dietiuk Kahraman and it is 
-        <a href="https://github.com/olgakahraman/weather-react"> open-sourced on GitHub</a>
+        Coded by Olga Dietiuk Kahraman and it is
+        <a href="https://github.com/olgakahraman/weather-react"
+         
+          target="_blank"
+          rel="noopener noreferrer">
+          open-sourced on GitHub
+        </a>
       </footer>
     </div>
   );
