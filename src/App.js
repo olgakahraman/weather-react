85e91fc54a1afc73287b401589772aa9c2be9934
olgakahraman <ol.khrmn@gmail.com>
2023-03-14 19:57:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="container">         
         <Weather defaultCity="New York" />
       </div>
       <footer className="text-center">
-        Coded by Olga Dietiuk Kahraman and it is
-        <a
+        Coded by Olga Dietiuk Kahraman and it is <br />
+        <strong>
+         <a
           href="https://github.com/olgakahraman/weather-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          open-sourced on GitHub
-        </a>
+       open-sourced on GitHub
+        </a></strong>
       </footer>
     </div>
   );
